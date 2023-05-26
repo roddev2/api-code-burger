8f0d6b31646ee7a6201068f971238098b9e97d94
rodolfomori <rod_dolpho@hotmail.com>
2023-05-25 22:02:27 -0300

    criacao da rota raiz

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,10 @@ const upload = multer(multerConfig)
 
 const routes = new Router()
 
+routes.get('/', (req,res)=>{
+    return res.json({message: 'Hello my first application'})
+})
+
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
