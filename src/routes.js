dbaf8e733cd48ec1406c3a23eb86640eeee680d9
rodolfomori <rod_dolpho@hotmail.com>
2023-05-25 22:14:38 -0300

    first commit

--- src/routes.js
+++ src/routes.js
@@ -14,10 +14,6 @@ const upload = multer(multerConfig)
 
 const routes = new Router()
 
-routes.get('/', (req,res)=>{
-    return res.json({message: 'Hello my first application'})
-})
-
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
