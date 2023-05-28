019db1cbf3acb57362d007e13cb0005c36fc12f1
rodolfomori <rod_dolpho@hotmail.com>
2023-05-28 09:56:09 -0300

    ajustes

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return `https://api-code-burger-production-3f8e.up.railway.app/product-file/${this.path}`
+            return `http://localhost:3001/product-file/${this.path}`
           },
         },
       },
